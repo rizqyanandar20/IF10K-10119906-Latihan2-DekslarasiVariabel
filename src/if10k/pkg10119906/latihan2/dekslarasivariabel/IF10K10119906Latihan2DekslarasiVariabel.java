8be71d4a6ab27529cfd47c65498e2e01ef1ced84
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 06:31:21 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan2/dekslarasivariabel/IF10K10119906Latihan2DekslarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan2.dekslarasivariabel;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Bentuk umum pendeklarasian variabel di java.
+ */
+public class IF10K10119906Latihan2DekslarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variable
+        int nilaiInt;
+        final double PHI = 3.14; // konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai Variable
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D'; 
+        
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
